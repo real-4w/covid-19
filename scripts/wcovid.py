c8ac68c4a3ae8688d596db97ae039de0dfa96561
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-05-09 11:35:45 +1200

    Quick comparison - needs more work.

--- scripts/wcovid.py
+++ scripts/wcovid.py
@@ -5,20 +5,23 @@ covid_df = pd.read_csv('../data/countries-aggregated.csv',parse_dates=['Date']) 
 nz_df = (covid_df.loc[covid_df['Country'] == "New Zealand"])
 nz_df = (nz_df.loc[covid_df['Confirmed'] > 0 ])
 
-us_df = (covid_df.loc[covid_df['Country'] == "US"])
-us_df = (us_df.loc[covid_df['Confirmed'] > 0 ])
-
 print('+-' * 30)
 print("Earliest infection NZ : ", nz_df['Date'].min())
 print("Latest data: ", nz_df['Date'].max())
 print(nz_df)
+country = input("Compare with what country: ")
+#Needs a check to see if country exits
+other_df = (covid_df.loc[covid_df['Country'] == country])
+other_df = (other_df.loc[covid_df['Confirmed'] > 0 ])
+
+
 print('+-' * 30)
-print("Earliest infection US : ", us_df['Date'].min())
-print("Latest data: ", us_df['Date'].max())
-print(us_df)
+print(f"Earliest infection {country}", other_df['Date'].min())
+print("Latest data: ", other_df['Date'].max())
+print(other_df)
 print('+-' * 30)
 
 nz_df.plot(x='Date', y=['Confirmed', 'Recovered', 'Deaths'])
-us_df.plot(x='Date', y=['Confirmed', 'Recovered', 'Deaths'])
+other_df.plot(x='Date', y=['Confirmed', 'Recovered', 'Deaths'])
 
 plt.show()
