eed767654671ede0e14e41f9bc1162a570816145
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-05-28 11:09:15 +1200

    Update main code

--- scripts/wworld.py
+++ scripts/wworld.py
@@ -4,5 +4,7 @@
 import pandas as pd                                         #needed for dataframe
 
 wview_df = pd.read_csv('../data/worldwide-aggregated.csv',parse_dates=['Date'])
-print('+-' * 30)
-print(wview_df)
+
+if __name__ == "__main__":    # execute only if run as a script
+    print('+-' * 30)
+    print(wview_df)
