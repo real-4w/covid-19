00321fc9990c1de5319b06a3c7868bbaf4d3645f
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-07-30 17:29:38 +1200

    Clean data up

--- scripts/wworld.py
+++ scripts/wworld.py
@@ -3,7 +3,7 @@
 #==============================================================================
 import pandas as pd                                         #needed for dataframe
 
-wview_df = pd.read_csv('../data/worldwide-aggregated.csv',parse_dates=['Date'])
+wview_df = pd.read_csv('./data/worldwide-aggregated.csv',parse_dates=['Date'])
 
 if __name__ == "__main__":    # execute only if run as a script
     print('+-' * 30)
